56d4595b22c449e19a6a93119ee98cdcbca276eb
cemah.alexandr <cemah.alexandr@gmail.com>
2016-09-25 22:24:47 +0300

    Переход меню в моб. вид при 840px. Размер формы в футере в %. Ссылки без index.php в навигации.

--- inc/nav.php
+++ inc/nav.php
@@ -16,14 +16,14 @@
 
       <ul class="nav navbar-nav navbar-left">
         <li class="dropdown">
-<!--          <a href="index.php#travels" style="display: inline-block;" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">-->
-          <a href="index.php#travels" style="display: inline-block;">
+<!--          <a href="/#travels" style="display: inline-block;" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">-->
+          <a href="/#travels" style="display: inline-block;">
             Путешествия
             <span class="caret " ></span>
           </a>
 
           <ul class="dropdown-menu">
-            <li><a tabindex="-1" href="index.php#travels">Путешествия</a></li>
+            <li><a tabindex="-1" href="/#travels">Путешествия</a></li>
             <li><a tabindex="-1" href="arctic.php">Арктические экспедиции</a></li>
             <li><a tabindex="-1" href="yachting.php">Яхтинг</a></li>
             <li><a tabindex="-1" href="adventure.php">Приключения</a></li>
@@ -31,10 +31,10 @@
             <li><a tabindex="-1" href="calendar.php">Календарь</a></li>
           </ul>
         </li>
-        <li><a href="#comunity">Сообщество</a></li>
-        <li><a href="#upcoming-event">Ближайшие экспедиции</a></li>
-        <li><a href="#reviews">Отзывы</a></li>
-        <li><a href="#contacts">Контакты</a></li>
+        <li><a href="/#comunity">Сообщество</a></li>
+        <li><a href="/#upcoming-event">Ближайшие экспедиции</a></li>
+        <li><a href="/#reviews">Отзывы</a></li>
+        <li><a href="/#contacts">Контакты</a></li>
 
       </ul>
       <ul class="nav navbar-nav navbar-right ul-nav-sotial">
